f1309bd01f7b56516aa91b8ef38869ea53b34173
Vildan Hakanaj <vildanhakanaj@gmail.com>
2022-12-23 18:34:16 -0500

    Added the options test file

--- tests/Unit/OptionsTest.php
+++ tests/Unit/OptionsTest.php
@@ -1,11 +1,18 @@
 <?php
 
-namespace Test;
+namespace Unit;
 
 use PHPUnit\Framework\TestCase;
 use VildanHakanaj\Options;
 
 class OptionsTest extends TestCase
 {
+
+    /**
+     * @test
+     */
+    public function it_can_accept_an_array_with_key_values_when_created(){
+
+    }
 
 }
